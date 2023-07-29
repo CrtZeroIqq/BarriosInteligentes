3af24ddb4edff64451aa048d1665d6e1d6cfe113
CrtZeroiqq <patricio.hp.iqq@gmail.com>
2023-07-29 10:33:09 -0400

    segunda carga

---
+++ app/src/main/java/com/example/bi_arica/DashboardActivity.kt
@@ -0,0 +1,4 @@
+package com.example.bi_arica
+
+class DashboardActivity {
+}
--- app/src/main/java/com/example/bi_arica/RegisterActivity.kt
+++ app/src/main/java/com/example/bi_arica/RegisterActivity.kt
@@ -7,11 +7,16 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.bi_arica.databinding.ActivityRegisterBinding
+import okhttp3.*
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import android.util.Log
+
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -39,7 +44,7 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.registerButton.setOnClickListener {
             if (validateFields()) {
-                // Your code to send data to your PHP API
+                registerUser()
             }
         }
     }
@@ -63,13 +68,8 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun formatRUT(rutField: EditText) {
         var rut = rutField.text.toString().replace("[.-]".toRegex(), "")
-        if (rut.length >= 8) {
+        if (rut.length >= 2) {
             rut = rut.substring(0, rut.length - 1) + "-" + rut.substring(rut.length - 1)
-            for (i in 4 downTo 1) {
-                if (rut.length > i + 2) {
-                    rut = rut.substring(0, rut.length - (i * 2 + 1)) + "." + rut.substring(rut.length - (i * 2 + 1))
-                }
-            }
 
             rutField.removeTextChangedListener(rutWatcher) //remove watcher
             rutField.setText(rut)
@@ -84,22 +84,27 @@ class RegisterActivity : AppCompatActivity() {
         val rutSplit = rut.replace("\\.", "").split("-").toTypedArray()
 
         if (rutSplit.size == 2) {
-            val number = rutSplit[0].toInt()
-            val dv = rutSplit[1].toUpperCase(Locale.ROOT)
+            try {
+                val number = rutSplit[0].toInt()
+                val dv = rutSplit[1].toUpperCase(Locale.ROOT)
 
-            var total = 0
-            var factor = 2
+                var total = 0
+                var factor = 2
 
-            for (i in number.toString().length - 1 downTo 0) {
-                total += Character.getNumericValue(number.toString()[i]) * factor
-                factor = if (factor == 7) 2 else factor + 1
-            }
+                for (i in number.toString().length - 1 downTo 0) {
+                    total += Character.getNumericValue(number.toString()[i]) * factor
+                    factor = if (factor == 7) 2 else factor + 1
+                }
 
-            val mod = total % 11
-            val ver = 11 - mod
+                val mod = total % 11
+                val ver = 11 - mod
 
-            if ((ver == 11 && dv == "0") || (ver == 10 && dv == "K") || ver.toString() == dv) {
-                valid = true
+                if ((ver == 11 && dv == "0") || (ver == 10 && dv == "K") || ver.toString() == dv) {
+                    valid = true
+                }
+            } catch (e: NumberFormatException) {
+                // La cadena RUT no se pudo convertir a un entero
+                valid = false
             }
         }
 
@@ -120,5 +125,53 @@ class RegisterActivity : AppCompatActivity() {
 
         return isValid
     }
+
+    private fun registerUser() {
+        val url = "http://54.227.125.166/barrios_inteligentes/assets/php/register.php"
+
+        val client = OkHttpClient()
+
+        val formBody = FormBody.Builder()
+            .add("first_name", binding.firstName.text.toString())
+            .add("last_name", binding.lastName.text.toString())
+            .add("rut", binding.rut.text.toString())
+            .add("birth_date", binding.birthDate.text.toString())
+            .add("email", binding.email.text.toString())
+            .add("phone_number", binding.phoneNumber.text.toString())
+            .add("neighborhood", binding.neighborhood.text.toString())
+            .add("password", binding.password.text.toString())
+            .add("password_confirmation", binding.passwordConfirmation.text.toString())
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(formBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseStr = response.body?.string()
+
+                    // update UI on the main thread
+                    runOnUiThread {
+                        Toast.makeText(this@RegisterActivity, "User registered successfully!", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    val errorMessage = response.message ?: "Unknown error"
+                    runOnUiThread {
+                        Toast.makeText(this@RegisterActivity, "Error: $errorMessage", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        })
+    }
+
+
 }
+
 
